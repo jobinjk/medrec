b816f9780fed2d4d743a60146324ccea16ae4d6c
Jobin <jobinj95@gmail.com>
2018-10-05 13:25:09 +0530

    added extension for adding mongo and other extension files

---
+++ medrec/extensions.py
@@ -0,0 +1,10 @@
+from passlib.hash import pbkdf2_sha256
+from flask_jwt_extended import JWTManager
+from flask_marshmallow import Marshmallow
+from flask_mongoengine import MongoEngine
+from celery import Celery
+
+db = MongoEngine()
+jwt = JWTManager()
+ma = Marshmallow()
+pwd_context = pbkdf2_sha256
