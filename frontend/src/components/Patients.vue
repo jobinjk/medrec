e1ca164378163a3fe11b7f7e91bdef8f94015cab
Jobin <jobinj95@gmail.com>
2018-10-11 09:15:00 +0530

    added changes in patients file to fix certain bugs like unwanted addition and deletion of medication line which occured in previous commit

--- frontend/src/components/Patients.vue
+++ frontend/src/components/Patients.vue
@@ -39,7 +39,7 @@
                 </v-tooltip>
               </td>
               <td>
-                <v-btn icon small @click.stop="editPatientView=true; $refs.editPatientForm.reset(); editPatient=props.item;buildEditMedications(props.item); error = {status:false,text:''}">
+                <v-btn icon small @click.stop="editPatientView=true; editPatient=props.item;buildEditMedications(props.item); error = {status:false,text:''}">
                   <v-icon small color="amber darken-1">fa-edit</v-icon>
                 </v-btn>
                 <v-btn icon small @click.native="deletePatient(props.item.id)">
@@ -74,22 +74,22 @@
                 <v-flex xs12 v-for="(med,index) in newPatientMedication" :key="index">
                   <v-layout row wrap>
                     <v-flex xs11>
-                    <v-text-field dark name="medication" solo label="Medication" placeholder="Enter Patients Medication" id="medication" v-model="med.item" required></v-text-field>
-                  </v-flex>
-                  <v-flex xs1>
-                    
-                  <v-btn color="error" dark icon small @click.native="newPatientMedication.splice(index,1)"><v-icon small>delete</v-icon></v-btn>
-                  
-                  </v-flex>
+                      <v-text-field dark name="medication" solo label="Medication" placeholder="Enter Patients Medication" id="medication" v-model="med.item" required></v-text-field>
+                    </v-flex>
+                    <v-flex xs1>
+
+                      <v-btn color="error" dark icon small @click.native="newPatientMedication.splice(index,1)"><v-icon small>delete</v-icon></v-btn>
+
+                    </v-flex>
                   </v-layout>
                 </v-flex>
-                    <v-btn color="teal" dark small @click.native="newPatientMedication.push({item:''})"><v-icon>add</v-icon>&nbsp; Add medication</v-btn>
-                  
+                <v-btn color="teal" dark small @click.native="newPatientMedication.push({item:''})"><v-icon>add</v-icon>&nbsp; Add medication</v-btn>
+
 
               </v-layout>
               <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn color="error darken-1" flat @click.native="addPatientView = false;">Cancel</v-btn>
+                <v-btn color="error darken-1" flat @click.native="newPatientMedication=[];addPatientView = false;">Cancel</v-btn>
                 <v-btn color="success darken-1" flat type="submit">Add</v-btn>
               </v-card-actions>
             </v-form>
@@ -107,12 +107,9 @@
           <v-card-text>You can edit a Patient here. </v-card-text>
           <v-card-text>
             <v-form v-model="editPatientForm" ref="editPatientForm" @submit.prevent="updatePatient" >
-
               <v-layout row wrap>
                 <v-flex xs12>
-
                   <v-text-field solo dark name="name" label="Name" placeholder="Enter Patients Name" id="name" v-model="editPatient.name" required></v-text-field>
-
                 </v-flex>
                 <v-flex xs12>
                   <v-text-field solo dark name="description" label="Description" placeholder="Enter Patients Description" id="description" v-model="editPatient.description" required></v-text-field>
@@ -121,23 +118,22 @@
                 <v-flex xs12 v-for="(med,index) in editPatientMedication" :key="index">
                   <v-layout row wrap>
                     <v-flex xs11>
-                    <v-text-field dark name="medication" solo label="Medication" placeholder="Enter Patients Medication" id="medication" v-model="med.item" required></v-text-field>
-                  </v-flex>
-                  <v-flex xs1>
-                    
-                  <v-btn color="error" dark icon small @click.native="editPatientMedication.splice(index,1)"><v-icon small>delete</v-icon></v-btn>
-                  
-                  </v-flex>
+                      <v-text-field dark name="medication" solo label="Medication" placeholder="Enter Patients Medication" id="medication" v-model="med.item" required></v-text-field>
+                    </v-flex>
+                    <v-flex xs1>
+                      <v-btn color="error" dark icon small @click.native="editPatientMedication.splice(index,1)">
+                        <v-icon small>delete</v-icon>
+                      </v-btn>
+                    </v-flex>
                   </v-layout>
                 </v-flex>
-                    <v-btn color="teal" dark small @click.native="editPatientMedication.push({item:''})"><v-icon>add</v-icon>&nbsp; Add medication</v-btn>
-                  
-
+                <v-btn color="teal" dark small @click.native="editPatientMedication.push({item:''})">
+                  <v-icon>add</v-icon>&nbsp; Add medication
+                </v-btn>
               </v-layout>
-
               <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn color="error darken-1" flat @click.native="editPatientView = false;">Cancel</v-btn>
+                <v-btn color="error darken-1" flat @click.native="editPatient.meds=[];editPatientView = false;">Cancel</v-btn>
                 <v-btn color="success darken-1" flat type="submit">Save</v-btn>
               </v-card-actions>
             </v-form>
@@ -184,7 +180,8 @@
           name: '',
           description: '',
           medication: '',
-          patients:[]
+          patients:[],
+          meds:[{item:''}]
         },
         patientHeaders: [
         {
@@ -222,7 +219,13 @@
           value: 'created_on',
           align: 'left',
           sortable: false
-        }
+        },
+        {
+            text: 'Actions',
+            value: 'actions',
+            align: 'left',
+            sortable: false
+          }
         ],
         patients: []
 
@@ -238,6 +241,10 @@
     },
 
     methods: {
+      appendNewMed(){
+        this.editPatient.meds.push({item:'new item'})
+        // console.log(this.editPatient)
+      },
       addPatient () {
         var url = '/api/patients'
         this.$http.post(url, this.newPatient).then(
@@ -264,11 +271,16 @@
           })
       },
       buildEditMedications(item){
+        this.editPatientMedication = []
         for (var i = item.medication.length - 1; i >= 0; i--) {
           this.editPatientMedication.push({item:item.medication[i]})
         }
       },
       updatePatient () {
+        this.editPatient.medication = []
+        for (var i = this.editPatientMedication.length - 1; i >= 0; i--) {
+          this.editPatient.medication.push(this.editPatientMedication[i].item)          
+        }
         var url = '/api/patients/' + this.editPatient.id
         this.$http.put(url, this.editPatient).then(
           (response) => {
@@ -294,6 +306,7 @@
       },
 
       fetchPatients () {
+        // console.log('>>>>>>>>>>>>>>>>>>>>')
         var url = ''
         var page = this.patientPagination.page
         var perPage = this.patientPagination.rowsPerPage
@@ -336,6 +349,7 @@
           })
       },
       created () {
+
 
       },
       beforeMount () {
