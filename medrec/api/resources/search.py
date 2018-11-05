42904475c84441467f1a2dcd877b2d494a6815dc
Jobin <jobinj95@gmail.com>
2018-11-05 17:21:25 +0530

    a search file for searching patient

---
+++ medrec/api/resources/search.py
@@ -0,0 +1,40 @@
+from .base import JWTResource
+from flask import jsonify, request, make_response
+from flask_jwt_extended import get_current_user
+from medrec.schemas import PatientsSchema
+from medrec.models import (
+    Patients as PatientsModel,
+    Users
+)
+from medrec.helpers import (
+    paginate,
+    loadconf
+)
+from mongoengine.errors import (
+    DoesNotExist, ValidationError
+)
+
+import os
+conf = loadconf()
+
+
+class Search(JWTResource):
+
+    def get(self):
+
+        schema = PatientsSchema(many=True)
+        data = request.args
+        current_user = get_current_user()
+
+        pid = data.get('id')
+
+        try:
+            patient = PatientsModel.objects(id=pid)
+
+        except (DoesNotExist, ValidationError):
+            return make_response(
+                jsonify(message='Patient not found'),
+                404
+            )
+
+        return paginate(patient, schema)
